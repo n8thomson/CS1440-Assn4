12e4fee0dc859d1fc90d7bac634c50e093880669
Nathan Thomson <nathan.thomson@aggiemail.usu.edu>
2018-11-29 19:27:05 -0700

    Assn5 Final

--- src/Gradient.py
+++ src/Gradient.py
@@ -5,30 +5,15 @@ class Gradient():
         raise NotImplementedError("Don't instantiate a Gradient!  Instantiate a subclass of Gradient!!!")
 
 
-    def getColor(self, n):
-        return self.colors[n - 1]
-
-    def getFirstColor(self):
-        return self.colors[0]
-
-    def getLastColor(self):
-        return self.colors[-1]
-
-    def getNumColors(self):
-        return len(self.colors)
 
     def linerlyInterpolate(self, steps, start, stop):
         dRed = (stop.r - start.r) / (steps - 1)
         dGrn = (stop.g - start.g) / (steps - 1)
         dBlu = (stop.b - start.b) / (steps - 1)
-        return list(
-            map(lambda n: Color(int(n * dRed) + start.r, int(n * dGrn) + start.g, int(n * dBlu) + start.b) , range(steps)))
+        return list(map(lambda n: Color(int(n * dRed) + start.r, int(n * dGrn) + start.g, int(n * dBlu) + start.b) , range(steps)))
 
     def getGradient(self, iterations):
         pass
-
-    def __str__(self):
-        return f"This is a gradient of {self.getNumColors()} colors: {self.colors}"
 
 
 
